13c0b20cf37c8c03ef53dc2fcba9c742411b65c6
masahiro_takeda <m-takeda731@outlook.jp>
2022-08-28 15:11:15 +0900

    ヘッダーの後ろに要素がきてしまう問題を修正するコードを追記

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,27 @@
-import { useRef } from 'react'
+import { useRef ,useEffect, useState} from 'react'
+import {useWindowSize} from 'react-use'
 import Logo from 'components/logo'
 import Nav from 'components/nav'
 import Container from 'components/container'
 import styles from 'styles/header.module.css'
 
 const Header = () => {
+  const headerRef = useRef<HTMLHeadElement>(null);
+
+  const [headerHeight, setHeaderHeight] = useState<number | undefined>(0);
+
+  const width = useWindowSize();
+  useEffect(() => {
+    setHeaderHeight(headerRef?.current?.offsetHeight)
+  },[width])
+
   return (
-    <header className={styles.header}>
+    <header className={styles.header} ref={headerRef}>
+      <style jsx global>{`
+      body {
+        margin-top: ${headerHeight}px;
+      }
+      `}</style>
       <Container large>
         <div className={styles.flexContainer}>
           <Logo />
