2ec1ead1108e869f4a0d16b4fc61a2a93c9b1add
masahiro_takeda <m-takeda731@outlook.jp>
2022-08-28 15:12:55 +0900

    コードのフォーマットを修正

--- components/header.tsx
+++ components/header.tsx
@@ -1,26 +1,26 @@
-import { useRef ,useEffect, useState} from 'react'
-import {useWindowSize} from 'react-use'
+import { useRef, useEffect, useState } from 'react'
+import { useWindowSize } from 'react-use'
 import Logo from 'components/logo'
 import Nav from 'components/nav'
 import Container from 'components/container'
 import styles from 'styles/header.module.css'
 
 const Header = () => {
-  const headerRef = useRef<HTMLHeadElement>(null);
+  const headerRef = useRef<HTMLHeadElement>(null)
 
-  const [headerHeight, setHeaderHeight] = useState<number | undefined>(0);
+  const [headerHeight, setHeaderHeight] = useState<number | undefined>(0)
 
-  const width = useWindowSize();
+  const width = useWindowSize()
   useEffect(() => {
     setHeaderHeight(headerRef?.current?.offsetHeight)
-  },[width])
+  }, [width])
 
   return (
     <header className={styles.header} ref={headerRef}>
       <style jsx global>{`
-      body {
-        margin-top: ${headerHeight}px;
-      }
+        body {
+          margin-top: ${headerHeight}px;
+        }
       `}</style>
       <Container large>
         <div className={styles.flexContainer}>
