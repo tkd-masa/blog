e260734d33e2850c2a93fac4a03b6547b3f80122
masahiro_takeda <m-takeda731@outlook.jp>
2022-08-30 18:06:57 +0900

    api.tsxとconstants.tsxを追加

---
+++ lib/constants.tsx
@@ -0,0 +1,9 @@
+export const siteMeta = {
+  siteTitle: 'TakeLog',
+  siteDesc: '日々学んだ技術に関して備忘録として記録していくサイト',
+  siteUrl: 'https://******',
+  siteLang: 'ja',
+  siteLocale: 'ja_JP',
+  siteType: 'website',
+  siteIcon: 'favicon.ico',
+}
