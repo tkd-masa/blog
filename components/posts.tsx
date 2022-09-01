d36a234dfd3a72a448b9c8b62ad61f2c5d4c9767
masahiro_takeda <m-takeda731@outlook.jp>
2022-09-01 12:39:59 +0900

    eyecatch画像を画面に表示する処理を追加

--- components/posts.tsx
+++ components/posts.tsx
@@ -1,17 +1,31 @@
 import styles from 'styles/posts.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 
 type Props = {
-    posts: { title: string; slug: string }[]
+    posts: { title: string; slug: string, eyecatch:{ url: string, width: number, height: number, blurDataURL: string} }[]
 }
 
 const Posts = ({ posts }: Props) => {
   return (
     <div className={styles.gridContainer}>
-      {posts.map(({ title, slug }: {title: string, slug: string}) => (
+      {posts.map(({ title, slug, eyecatch }: {title: string, slug: string, eyecatch:{ url: string, width: number, height: number, blurDataURL: string}}) => (
         <article className={styles.post} key={slug}>
           <Link href={`/${slug}`}>
             <a>
+              <figure>
+                <Image
+                  src={eyecatch.url}
+                  alt=""
+                  layout="fill"
+                  objectFit="cover"
+                  width={eyecatch.width}
+                  height={eyecatch.height}
+                  sizes="(min-width: 1152px) 576px, 50vw"
+                  placeholder="blur"
+                  blurDataURL={eyecatch.blurDataURL}
+                />
+              </figure>
               <h2>{title}</h2>
             </a>
           </Link>
