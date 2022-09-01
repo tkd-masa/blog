d8918472751abbfba564659f5de730edf3bc255f
masahiro_takeda <m-takeda731@outlook.jp>
2022-09-01 14:07:46 +0900

    getAllCategoriesを追加

--- lib/api.tsx
+++ lib/api.tsx
@@ -47,3 +47,19 @@ export const getAllPosts = async (limit = 100) => {
     console.log(err)
   }
 }
+
+export const getAllCategories = async (limit = 100) => {
+  try {
+    const categories = await client.get({
+      endpoint: 'categories',
+      queries: {
+        fields: 'name,id,slug',
+        limit: limit
+      },
+    })
+    return categories.contents
+  } catch (err) {
+    console.log('!-- getAllCategories --')
+    console.log(err)
+  }
+}
