09098cc402bb89949bfb8b802e1b8aaa48e5c300
masahiro_takeda <m-takeda731@outlook.jp>
2022-09-01 20:05:47 +0900

    _document.jsの拡張子をtsxに変更、googlefontのリンクを追記

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+import { siteMeta } from 'lib/constants'
+const { siteLang } = siteMeta
+
+const Document = () => {
+  return (
+    <Html lang={siteLang}>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet" />
+      </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+    </Html>
+  )
+}
+
+export default Document
