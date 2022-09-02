be5693e316d0f8a81a6cff64c6293e3d10e9cd04
masahiro_takeda <m-takeda731@outlook.jp>
2022-09-02 16:04:23 +0900

    投稿日とタイトルの位置を入れ替える処理を実施

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -13,13 +13,13 @@ const PostHeader = ({ title, subtitle, publish = '' }: Props) => {
   return (
     <div className={styles.stack}>
       <p className={styles.subtitle}>{subtitle}</p>
-      <h1 className={styles.title}>{title}</h1>
       {publish && (
         <div className={styles.publish}>
           <FontAwesomeIcon icon={faClock} size="lg" color="var(--black)" />
           <ConvertDate dateISO={publish} />
         </div>
       )}
+      <h1 className={styles.title}>{title}</h1>
     </div>
   )
 }
