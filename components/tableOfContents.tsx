58a2c2c747fd3092e5f310e1fecb0daae43ff210
masahiro_takeda <m-takeda731@outlook.jp>
2022-09-06 10:43:26 +0900

    目次を作成するコンポーネントを新規作成

---
+++ components/tableOfContents.tsx
@@ -0,0 +1,31 @@
+import styles from 'styles/tableOfContents.module.css'
+import { Link as Scroll } from 'react-scroll';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookOpen } from '@fortawesome/free-solid-svg-icons'
+
+type Props = {
+    toc: {
+        text: string | undefined,
+        id: string
+    }[]
+}
+
+const tableOfContents = ({toc}: Props) => {
+    return (
+        <div className={styles.toc}>
+            <p className={styles.tocHead}><FontAwesomeIcon icon={faBookOpen} />目次</p>
+            <ol>
+            {toc.map(data => (
+                <li key={data.id}>
+                {/* <a href={`#${data.id}`}>
+                    {data.text}
+                </a> */}
+                <Scroll to={`${data.id}`} smooth={true} duration={700} offset={-120}>{data.text}</Scroll>
+                </li>
+            ))}
+            </ol>
+        </div>
+    );
+}
+
+export default tableOfContents
