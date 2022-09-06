601cda15bd0149e41b483f2be7458ba08a0146f6
masahiro_takeda <m-takeda731@outlook.jp>
2022-09-06 10:44:47 +0900

    react-scrollで目次にページ内リンクを作成する作業を実施

--- components/tableOfContents.tsx
+++ components/tableOfContents.tsx
@@ -1,31 +1,33 @@
 import styles from 'styles/tableOfContents.module.css'
-import { Link as Scroll } from 'react-scroll';
+import { Link as Scroll } from 'react-scroll'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookOpen } from '@fortawesome/free-solid-svg-icons'
 
 type Props = {
-    toc: {
-        text: string | undefined,
-        id: string
-    }[]
+  toc: {
+    text: string | undefined
+    id: string
+  }[]
 }
 
-const tableOfContents = ({toc}: Props) => {
-    return (
-        <div className={styles.toc}>
-            <p className={styles.tocHead}><FontAwesomeIcon icon={faBookOpen} />目次</p>
-            <ol>
-            {toc.map(data => (
-                <li key={data.id}>
-                {/* <a href={`#${data.id}`}>
-                    {data.text}
-                </a> */}
-                <Scroll to={`${data.id}`} smooth={true} duration={700} offset={-120}>{data.text}</Scroll>
-                </li>
-            ))}
-            </ol>
-        </div>
-    );
+const tableOfContents = ({ toc }: Props) => {
+  return (
+    <div className={styles.toc}>
+      <p className={styles.tocHead}>
+        <FontAwesomeIcon icon={faBookOpen} />
+        目次
+      </p>
+      <ol>
+        {toc.map((data) => (
+          <li key={data.id}>
+            <Scroll to={`${data.id}`} smooth={true} duration={700} offset={-120}>
+              {data.text}
+            </Scroll>
+          </li>
+        ))}
+      </ol>
+    </div>
+  )
 }
 
 export default tableOfContents
