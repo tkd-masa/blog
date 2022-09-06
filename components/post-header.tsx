adc6040f9e1661619bb0b2c2be5e867e30e872cc
masahiro_takeda <m-takeda731@outlook.jp>
2022-09-06 13:35:45 +0900

    subtitleをtagに変更する処理を実施

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -5,14 +5,14 @@ import styles from 'styles/post-header.module.css'
 
 type Props = {
   title: string
-  subtitle: string
+  tag: string[]
   publish?: string
 }
 
-const PostHeader = ({ title, subtitle, publish = '' }: Props) => {
+const PostHeader = ({ title, tag, publish = '' }: Props) => {
   return (
     <div className={styles.stack}>
-      <p className={styles.subtitle}>{subtitle}</p>
+      <p className={styles.tag}>{tag}</p>
       {publish && (
         <div className={styles.publish}>
           <FontAwesomeIcon icon={faClock} size="lg" color="var(--gray-50)" />
