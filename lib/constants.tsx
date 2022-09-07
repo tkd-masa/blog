4124347d9d53b2e96fb123a568250621cb813979
masahiro_takeda <m-takeda731@outlook.jp>
2022-09-07 12:44:26 +0900

    siteMetaのsiteUrlを変更

--- lib/constants.tsx
+++ lib/constants.tsx
@@ -1,7 +1,7 @@
 export const siteMeta = {
   siteTitle: 'TakeLog',
   siteDesc: '日々学んだ技術に関して備忘録として記録していくサイト',
-  siteUrl: 'https://******',
+  siteUrl: 'https://takelog.vercel.app/',
   siteLang: 'ja',
   siteLocale: 'ja_JP',
   siteType: 'website',
