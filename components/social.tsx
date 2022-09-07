77355c006b09afb15b787eb844d20b7b7327e962
masahiro_takeda <m-takeda731@outlook.jp>
2022-09-07 15:59:28 +0900

    twitterのリンクを変更。facebookのアイコンを削除

--- components/social.tsx
+++ components/social.tsx
@@ -1,25 +1,19 @@
 import styles from 'styles/social.module.css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTwitter, faFacebookF, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Social = ({ iconSize = 'initial' }) => {
   return (
     <ul className={styles.list} style={{ ['--icon-size' as any]: iconSize }}>
       <li>
-        <a href="#">
+        <a href="https://twitter.com/takelog_tech">
           <FontAwesomeIcon icon={faTwitter} />
           <span className="sr-only">Twitter</span>
         </a>
       </li>
       <li>
-        <a href="#">
-          <FontAwesomeIcon icon={faFacebookF} />
-          <span className="sr-only">Facebook</span>
-        </a>
-      </li>
-      <li>
-        <a href="#">
+        <a href="https://github.com/tkd-masa/blog">
           <FontAwesomeIcon icon={faGithub} />
           <span className="sr-only">GitHub</span>
         </a>
