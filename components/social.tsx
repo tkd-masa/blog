b4795173ed923dd036c723c526e5c5334c7cd84a
masahiro_takeda <m-takeda731@outlook.jp>
2022-09-07 16:05:00 +0900

    target="_blank"の追記

--- components/social.tsx
+++ components/social.tsx
@@ -7,13 +7,13 @@ const Social = ({ iconSize = 'initial' }) => {
   return (
     <ul className={styles.list} style={{ ['--icon-size' as any]: iconSize }}>
       <li>
-        <a href="https://twitter.com/takelog_tech">
+        <a href="https://twitter.com/takelog_tech" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faTwitter} />
           <span className="sr-only">Twitter</span>
         </a>
       </li>
       <li>
-        <a href="https://github.com/tkd-masa/blog">
+        <a href="https://github.com/tkd-masa/blog" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faGithub} />
           <span className="sr-only">GitHub</span>
         </a>
